c616df4df480be3efdafe6d040400ccdef30c199
Thomas Bouvier <tomatrocho@gmail.com>
2016-10-24 11:46:03 +0200

    Added lab 3

---
+++ C2/TP3/fichier.h
@@ -0,0 +1,57 @@
+/*!
+ * \file fichier.h
+ * \brief declaraion des fonctions associees a un fichier image
+ * \date ...
+ */
+
+#ifndef FICHIER_H
+#define FICHIER_H
+
+#include <stdint.h>
+#include <math.h>
+
+#include "image.h"
+
+/**
+To avoid padding (issue with sizeof(FileHeader) otherwise -> if padding enabled, replace sizeof(FileHeader) with the value 14 )
+*/
+#pragma pack(1)
+
+/** A struct typedef to store the content of the BMP file header
+*/
+typedef struct {
+    char identity[2];              /*!<"BM" for a Windows BitMap*/
+    uint32_t  file_size;           /*!<4 bytes for an unsigned int*/
+    char application_id[4];        /*!<4 bytes for the id of the application which creates the file*/
+    uint32_t raster_address;       /*!<4 bytes corresponding to the adress in the file where the image data begins*/
+} FileHeader;
+
+
+/** A struct typedef to store the content of the DIB  header
+*/
+typedef struct {
+    uint32_t size_DIBHeader;      /*!<DIB Header size*/
+    uint32_t image_width;         /*!<Image width in pixels = number of columns*/
+    uint32_t image_height;        /*!<Image height in pixels = number of rows*/
+    uint16_t nbColorPlanes;       /*!<set to 1*/
+    uint16_t nbBitPerPixel;       /*!<set to 24 for the practical work (3 bytes for RGB)*/
+    uint32_t typeCompression;     /*!<set to  0 for RGB colour space*/
+    uint32_t size_raw_image;      /*!<total image data size = nb pixel * size of a pixel*/
+    int32_t hResolution;          /*!<horizontal resolution. Equals to  2800 fo this PW*/
+    int32_t vResolution;          /*!<vertical resolution. Equals to  2800 fo this PW*/
+    uint32_t nbUsedColours;       /*!<set to 0 for this PW*/
+    uint32_t nbImportantColours;  /*!<set to 0 for this PW*/
+} DIBHeader;
+
+void displayFileHeader(FileHeader * eFichier);
+void displayDIBHeader(DIBHeader * eImage);
+
+void readFileHeader(FILE * fp, FileHeader * eFichier);
+void readDIBHeader(FILE * fp,  DIBHeader * eImage);
+
+Image* readRawImage(FILE * fp, unsigned int adresse, int l, int h);
+Image* readBMPFile(char * nomFichier, int verbose);
+
+void writeBMPFile(char * nomFichier, Image * image, int verbose);
+
+#endif
---
+++ C2/TP3/image.c
@@ -0,0 +1,36 @@
+#include "image.h"
+
+int setPixelxy(Image * image, int x, int y , Pixel p) {
+    if (x < 0 || x > image->width - 1 || y < 0 || y > image->height - 1)
+        return -1;
+
+    image->raw_data[y * image->width + x] = p;
+
+    return 0;
+}
+
+Pixel getPixelxy(Image * image, int x, int y) {
+    if (x < 0 || x > image->width - 1 || y < 0 || y > image->height - 1) {
+        Pixel blackPixel;
+
+        blackPixel.red = 0;
+        blackPixel.green = 0;
+        blackPixel.blue = 0;
+
+        return blackPixel;
+    }
+
+    return image->raw_data[y * image->width + x];
+}
+
+Image * allocateImage(Image * i, int h, int w) {
+    i->height = h;
+    i->width = w;
+    i->raw_data = (Pixel *) malloc(i->height * i->width * sizeof(Pixel));
+
+    return i;
+}
+
+void freeImage(Image * i) {
+    free(i->raw_data);
+}
