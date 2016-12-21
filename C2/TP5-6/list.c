e5a7bf354ffc1faad3a4fccbaa46d13e6278a417
Thomas Bouvier <tomatrocho@gmail.com>
2016-12-21 20:13:47 +0100

    Added insert functions and deleteCurrent() from lab 6

--- C2/TP5-6/list.c
+++ C2/TP5-6/list.c
@@ -239,7 +239,7 @@ int deleteLast(List * l, int * v) {
     NodeList * previous = NULL;
 
     if (!empty(l)) {
-        if (v != NULL)
+        if (v)
             *v = nToDel->val;
 
         setOnFirst(l);
@@ -265,6 +265,98 @@ int deleteLast(List * l, int * v) {
     return 0;
 }
 
+/*!
+* Insert a new element containing the int value given as a parameter after the current element of the list.
+* \param[out] l the list
+* \param[in] v the value to be inserted
+* \return int 1 if the element was successfully inserted, 0 otherwise
+*/
+int insertAfterCurrent(List * l, int v) {
+    NodeList * new_n = NULL;
+
+    if (!outOfList(l)) {
+      new_n = newNodeList(v, l->current->next);
+
+      l->current->next = new_n;
+
+      if (l->current == l->last)
+          l->last = l->current->next;
+
+      return 1;
+    }
+
+    return 0;
+}
+
+/*!
+* Insert a new element containing the int value given as a parameter before the current element of the list.
+* \param[out] l the list
+* \param[in] v the value to be inserted
+* \return int 1 if the element was successfully inserted, 0 otherwise
+*/
+int insertBeforeCurrent(List * l, int v) {
+  NodeList * new_n = NULL;
+  NodeList * previous = NULL;
+  NodeList * stop = NULL;
+
+  if (!outOfList(l)) {
+    new_n = newNodeList(v, l->current);
+    stop = l->current;
+
+    setOnFirst(l);
+    while (l->current != stop) {
+      previous = l->current;
+      next(l);
+    }
+
+    if (l->current == l->first)
+        insertFirst(l, v);
+    else
+        previous->next = new_n;
+
+    return 1;
+  }
+
+  return 0;
+}
+
+/*!
+* Delete the current element of the list. Free the memory of the delete list node.
+* \param[out] l the list
+* \param[out] v an address to store the deleted value. If null, no storage is performed.
+* \return int 1 if the element was successfully deleted, 0 otherwise
+*/
+int deleteCurrent(List * l, int * v) {
+  NodeList * previous = NULL;
+  NodeList * stop = NULL;
+
+    if (!outOfList(l)) {
+      if (l->current == l->first)
+          deleteFirst(l, v);
+      else if (l->current == l->last)
+          deleteLast(l, v);
+      else {
+          if (v)
+              *v = l->current->val;
+
+          stop = l->current;
+
+          setOnFirst(l);
+          while (l->current != stop) {
+            previous = l->current;
+            next(l);
+          }
+
+          previous->next = l->current->next;
+          freeNodeList(l->current);
+      }
+
+      return 1;
+    }
+
+    return 0;
+}
+
 void test_list() {
     int ret1,ret2;
     int val;
@@ -283,13 +375,13 @@ void test_list() {
 
     /*group 3*/
 
-    display_test_check_by_user("Test printList on an empty list");
+    display_test_check_by_user("Test printList() on an empty list");
     printList(&l1);
     ret1 = insertFirst(&l1, 0);
     ret2 = insertFirst(&l1, 1);
     display_test_int("Test insertFirst() on an empty list", 1, ret1);
     display_test_int("Test insertFirst() on a not-empty list", 1, ret2);
-    display_test_check_by_user("Test insertFirst : (list should be 1 0)");
+    display_test_check_by_user("Test insertFirst() : (list should be 1 0)");
     printList(&l1);
 
     display_test_int("Test empty() on a not-empty list", 0, empty(&l1));
@@ -312,21 +404,21 @@ void test_list() {
     ret1 = deleteFirst(&l1, &val);
     display_test_int("Test deleteFirst() on a not-empty list", 1, ret1);
     display_test_int("Test deleteFirst() on a not-empty list", 1, val);
-    display_test_check_by_user("Test deleteFirst : (list should be 0)");
+    display_test_check_by_user("Test deleteFirst() : (list should be 0)");
     printList(&l1);
 
     ret1 = deleteFirst(&l1, NULL);
-    display_test_check_by_user("Test deleteFirst with only an element in the list : (list should be empty)");
+    display_test_check_by_user("Test deleteFirst() with only an element in the list : (list should be empty)");
     printList(&l1);
 
     ret1 = deleteFirst(&l1, NULL);
     display_test_int("Test deleteFirst() on an empty list", 0, ret1);
 
     freeList(&l1);
-    display_test_check_by_user("Test freeList on a not-empty list : (list should be empty)");
+    display_test_check_by_user("Test freeList() on a not-empty list : (list should be empty)");
     printList(&l1);
     freeList(&l2);
-    display_test_check_by_user("Test freeList on an empty list : (list should be empty)");
+    display_test_check_by_user("Test freeList() on an empty list : (list should be empty)");
     printList(&l2);
 
     /*group 5*/
@@ -335,21 +427,95 @@ void test_list() {
     ret2 = insertLast(&l2, 1);
     display_test_int("Test insertLast() on an empty list", 1, ret1);
     display_test_int("Test insertLast() on a not-empty list", 1, ret2);
-    display_test_check_by_user("Test insertLast : (list should be 0 1)");
+    display_test_check_by_user("Test insertLast() : (list should be 0 1)");
     printList(&l2);
 
     ret1 = deleteLast(&l2, &val);
     display_test_int("Test deleteLast() on a not-empty list", 1, ret1);
     display_test_int("Test deleteLast() on a not-empty list", 1, val);
-    display_test_check_by_user("Test deleteLast : (list should be 0)");
+    display_test_check_by_user("Test deleteLast() : (list should be 0)");
     printList(&l2);
 
     ret1 = deleteLast(&l2, NULL);
-    display_test_check_by_user("Test deleteLast with only an element in the list : (list should be empty)");
+    display_test_check_by_user("Test deleteLast() with only an element in the list : (list should be empty)");
     printList(&l2);
 
     ret1 = deleteLast(&l2, NULL);
     display_test_int("Test deleteLast() on an empty list", 0, ret1);
+
+    /*group 6*/
+    // both lists are empty
+
+    ret1 = insertAfterCurrent(&l1, 10);
+    display_test_int("Test insertAfterCurrent() on an empty list", 0, ret1);
+
+    insertLast(&l2, 0);
+    insertLast(&l2, 2);
+    insertLast(&l2, 4);
+    insertLast(&l2, 6);
+    setOnFirst(&l2);
+    next(&l2);
+
+    ret2 = insertAfterCurrent(&l2, 10);
+    display_test_check_by_user("Test insertAfterCurrent() on a non-empty list : (list should be 0 2 10 4 6)");
+    printList(&l2);
+
+    setOnFirst(&l2);
+    next(&l2);
+    next(&l2);
+    next(&l2);
+    next(&l2);
+
+    ret2 = insertAfterCurrent(&l2, 10);
+    display_test_check_by_user("Test insertAfterCurrent() on a non-empty list : (list should be 0 2 10 4 6 10)");
+    printList(&l2);
+
+    ret1 = insertBeforeCurrent(&l1, 10);
+    display_test_int("Test insertBeforeCurrent() on an empty list", 0, ret1);
+
+    setOnFirst(&l2);
+    next(&l2);
+
+    ret2 = insertBeforeCurrent(&l2, 10);
+    display_test_check_by_user("Test insertBeforeCurrent() on a non-empty list : (list should be 0 10 2 10 4 6 10)");
+    printList(&l2);
+
+    setOnFirst(&l2);
+
+    ret2 = insertBeforeCurrent(&l2, 8);
+    display_test_check_by_user("Test insertBeforeCurrent() on a non-empty list : (list should be 8 0 10 2 10 4 6 10)");
+    printList(&l2);
+
+    ret1 = deleteCurrent(&l1, NULL);
+    display_test_int("Test deleteCurrent() on an empty list", 0, ret1);
+
+    setOnFirst(&l2);
+    next(&l2);
+    next(&l2);
+    next(&l2);
+    next(&l2);
+    next(&l2);
+
+    ret2 = deleteCurrent(&l2, NULL);
+    display_test_check_by_user("Test deleteCurrent() on a non-empty list : (list should be 8 0 10 2 10 6 10)");
+    printList(&l2);
+
+    setOnFirst(&l2);
+
+    ret2 = deleteCurrent(&l2, NULL);
+    display_test_check_by_user("Test deleteCurrent() on a non-empty list : (list should be 0 10 2 10 6 10)");
+    printList(&l2);
+
+    setOnFirst(&l2);
+    next(&l2);
+    next(&l2);
+    next(&l2);
+    next(&l2);
+    next(&l2);
+
+    ret2 = deleteCurrent(&l2, NULL);
+    display_test_check_by_user("Test deleteCurrent() on a non-empty list : (list should be 0 10 2 10 6)");
+    printList(&l2);
 
 	  /*Always free the dynamically allocated memory*/
 
